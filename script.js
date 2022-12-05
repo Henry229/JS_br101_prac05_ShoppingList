6948c91c1b1f937e3d28015e55478b5662bcda1d
Henry Chun <chunhyungjoon@gmail.com>
2022-12-05 11:37:41 +1100

    built html, css

---
+++ script.js
@@ -0,0 +1,15 @@
+// const item = document.querySelector('.input');
+// const returnValue = item.addEventListener('submit', inputValue);
+
+// const inputValue = (event) => {
+//   event.preventDefault();
+//   const value = event.target.value;
+//   return value;
+//   // if (value === '') { inputValue()}
+// };
+
+// const list = document.querySelector('.list');
+// list.innerHTML += `<div class='item'>${returnValue}</div> \n
+//                   <div class="bin">
+//                     <i class="far fa-trash-alt"></i>
+//                   </div>`;
